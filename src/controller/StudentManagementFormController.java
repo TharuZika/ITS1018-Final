817a6df8b24cbee979cc18b3bb00be371a6678dd
TharuZika <maxtharuz4@gmail.com>
2022-07-12 10:30:46 +0530

    Create entity

--- src/controller/StudentManagementFormController.java
+++ src/controller/StudentManagementFormController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class StudentManagementFormController {
+    public JFXTextField txtStudentID;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNic;
+    public TableView tblStudents;
+    public TableColumn colStudentId;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void addStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteStudentOnAction(ActionEvent actionEvent) {
+    }
 }
