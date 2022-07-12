b2628a4beace3619ff97bbea4687f214c9c8467a
TharuZika <maxtharuz4@gmail.com>
2022-07-12 12:09:25 +0530

    Add CRUD for student

--- src/controller/StudentManagementFormController.java
+++ src/controller/StudentManagementFormController.java
@@ -1,11 +1,14 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.Student;
@@ -44,6 +47,34 @@ public class StudentManagementFormController {
             e.printStackTrace();
         }
 
+        tblStudents.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if (tblStudents.getSelectionModel().getSelectedItem() != null){
+                    TableView.TableViewSelectionModel selectionModel = tblStudents.getSelectionModel();
+                    ObservableList selectionCells = selectionModel.getSelectedCells();
+                    TablePosition tablePosition = (TablePosition) selectionCells.get(0);
+                    Object val = tablePosition.getTableColumn().getCellData(newValue);
+
+                    try {
+                        ResultSet resultSet = CrudUtil.execute("SELECT * FROM student WHERE student_id=?", val);
+                        while (resultSet.next()){
+                            txtStudentID.setText(resultSet.getString(1));
+                            txtStudentName.setText(resultSet.getString(2));
+                            txtEmail.setText(resultSet.getString(3));
+                            txtContact.setText(resultSet.getString(4));
+                            txtAddress.setText(resultSet.getString(5));
+                            txtNic.setText(resultSet.getString(6));
+                        }
+                    } catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
 
     public void loadAllStudents() throws SQLException, ClassNotFoundException {
@@ -66,11 +97,77 @@ public class StudentManagementFormController {
     }
 
     public void addStudentOnAction(ActionEvent actionEvent) {
+        if (txtStudentID.getText().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please insert all data !").show();
+        }else {
+            Student stud = new Student(
+                    txtStudentID.getText(), txtStudentName.getText(), txtEmail.getText(), txtContact.getText(), txtAddress.getText(), txtNic.getText()
+            );
+            try {
+                if (CrudUtil.execute("INSERT INTO student VALUES (?, ?, ?, ?, ?, ?)", stud.getStudent_id(), stud.getStudent_name(), stud.getEmail(), stud.getContact(), stud.getAddress(), stud.getNic())) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Details are Saved !").show();
+                }else {
+                    new Alert(Alert.AlertType.WARNING, "Details are not Saved !").show();
+                }
+            } catch (SQLException | ClassNotFoundException e){
+                e.printStackTrace();
+                new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+            }
+            try {
+                loadAllStudents();
+            } catch (ClassNotFoundException | SQLException e){
+                e.printStackTrace();
+            }
+            clearFields();
+        }
     }
 
     public void updateStudentOnAction(ActionEvent actionEvent) {
+        Student upStud = new Student(
+                txtStudentID.getText(), txtStudentName.getText(), txtEmail.getText(), txtContact.getText(), txtAddress.getText(), txtNic.getText()
+        );
+        try {
+            if (CrudUtil.execute("UPDATE student SET student_name=? , email=? , contact=? , address=? , nic=? WHERE student_id=?",  upStud.getStudent_name(), upStud.getEmail(), upStud.getContact(), upStud.getAddress(), upStud.getNic(), upStud.getStudent_id())) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Details are Updated !").show();
+            }else {
+                new Alert(Alert.AlertType.WARNING, "Didn't Updated !").show();
+            }
+        } catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+
+        try {
+            loadAllStudents();
+        } catch (ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+        clearFields();
     }
 
     public void deleteStudentOnAction(ActionEvent actionEvent) {
+        try {
+            if (CrudUtil.execute("DELETE FROM student WHERE student_id=?", txtStudentID.getText())){
+               new Alert(Alert.AlertType.CONFIRMATION, "Deleted..!").show();
+               clearFields();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clearOnAction(ActionEvent actionEvent) {
+        clearFields();
+    }
+
+    private void clearFields(){
+        txtContact.clear();
+        txtNic.clear();
+        txtAddress.clear();
+        txtStudentName.clear();
+        txtStudentID.clear();
+        txtEmail.clear();
     }
 }
